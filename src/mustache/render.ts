ee40e1fb1f518f85225b0d0d05e55be4282edc94
Tag Howard <tag.howard1080@gmail.com>
2022-07-18 21:53:23 -0400

    Add logging to render.ts

--- src/mustache/render.ts
+++ src/mustache/render.ts
@@ -1,16 +1,21 @@
 import { readFileSync } from "fs";
 import Mustache from "mustache";
+import { log } from "../util";
 import { Rules } from "./rules";
 
 export function renderSchema(schema: Rules): string {
+  log(`Loading base template as ${__dirname + "\\root.mustache"}`);
   return Mustache.render(
     readFileSync(
       __dirname + "\\root.mustache", "utf8"
     ),
     schema,
-    (name) => readFileSync(
-      __dirname + "\\" + name + ".mustache", "utf8"
-    ),
+    (name) => {
+      log(`Loading template (${name}) as ${__dirname + "\\" + name + ".mustache"}`);
+      return readFileSync(
+        __dirname + "\\" + name + ".mustache", "utf8"
+      )
+    },
     {
       escape: (val) => val
     }
