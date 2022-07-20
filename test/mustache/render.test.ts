459f66146d3296a48a2b70e23b1c16480f2d2396
Tag Howard <tag.howard1080@gmail.com>
2022-07-20 18:36:59 -0400

    Add a placeholder for render tests

---
+++ test/mustache/render.test.ts
@@ -0,0 +1,7 @@
+import { renderSchema } from "../../src/mustache/render";
+
+describe(renderSchema, () => {
+  it.todo("should render a valid rules object correctly");
+
+  it.todo("should render an empty object correctly (sorta)");
+});
